7bb27c7efa9597ea856fd2e197d6a946f9f5acbe
Shaishav Paudel <morganhouse15@gmail.com>
2023-06-25 09:27:43 +0545

    ReactiveRxjsLeelaYoutubeVideo29-routingPopulateValuesForUpdateCompleted

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  UntypedFormControl,
+  UntypedFormGroup,
+} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, map, tap } from 'rxjs';
 import { DeclarativeCategoryService } from 'src/app/services/declarative-category.service';
+import { DeclarativePostService } from 'src/app/services/declarative-post.service';
 
 @Component({
   selector: 'app-post-form',
@@ -7,7 +16,44 @@ import { DeclarativeCategoryService } from 'src/app/services/declarative-categor
   styleUrls: ['./post-form.component.scss'],
 })
 export class PostFormComponent {
+  postForm = new UntypedFormGroup({
+    title: new UntypedFormControl(''),
+    description: new UntypedFormControl(''),
+    categoryId: new UntypedFormControl(''),
+  });
+  selectedPostId$ = this.route.paramMap.pipe(
+    map((paramMap) => {
+      const id = paramMap.get('id')!;
+      this.postService.selectPost(id);
+      return id;
+    })
+  );
+
+  post$ = this.postService.post$.pipe(
+    tap((post) => {
+      post &&
+        this.postForm.setValue({
+          title: post?.title,
+          description: post?.description,
+          categoryId: post?.categoryId,
+        });
+    })
+  );
   categories$ = this.categoryService.categories$;
 
-  constructor(private categoryService: DeclarativeCategoryService) {}
+  vm$ = combineLatest([this.selectedPostId$, this.post$]);
+
+  // ngOnInit() {
+  //   console.log(this.route);
+  // }
+
+  constructor(
+    private categoryService: DeclarativeCategoryService,
+    private route: ActivatedRoute, // private ref: ChangeDetectorRef
+    private postService: DeclarativePostService
+  ) {}
+
+  onPostSubmit() {
+    console.log(this.postForm.value);
+  }
 }
